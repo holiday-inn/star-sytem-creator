6e9c5d246a3b2a04c2dcab9cde1e9a538fbe6e18
holiday-inn <113055314+holiday-inn@users.noreply.github.com>
2023-11-21 12:26:12 -0800

    completed revised

--- respectablefilename.py
+++ respectablefilename.py
@@ -56,6 +56,9 @@ class star():
         self.lawlvl = (random.randint(1,6) + random.randint(1,6) - 7 + self.govt)
         if self.atm <= 0: self.atm = 0
         if self.lawlvl < 0: self.lawlvl = 0
+        if self.lawlvl > 10: self.lawlvl = 10
+        if self.pop > 9: self.pop = 9
+        if self.govt > 13: self.govt = 13
         
         line = open("tables.csv","r").read().split("\n")
             
@@ -143,16 +146,10 @@ def main():
         cursor.execute(query)
         result = cursor.fetchall()
         
-        for i in (result):
-            for k in data:
-                temp = data.index(k)+1
-                if temp%2 == 0:
-                    print(f"\033[1m{k}\033[0m: {i[data.index(k)+1]}")
-                else: print(f"\033[1m{k:<20}\033[0m: {i[data.index(k)+1]:<20}",end="")
-            print("\n")
+        
     
         choice = input('\n\033[1mWhat would you like to do? (help for options): \033[0m ')
-        if choice == 'help':print("\nYou can: \nlook: To look at star systems\ngenerate: To create new star systems")
+        if choice == 'help':print("\nYou can: \nlook: To look at star systems or search systems for values\ngenerate: To create new star systems");continue
         
         elif choice == 'look':
             inp = input("\n\033[1mWould you like to look for name, size, atm, hyd, pop, \ngovt, law, tech, starport, naval, scout, gas, plane or all?: \033[0m").strip()
@@ -170,22 +167,58 @@ def main():
                 if len(result) == 0:
                     print("\033[1mNo data with that value\033[0m ")
                     continue
+                for i in (result):
+                    for k in data:
+                        temp = data.index(k)+1
+                        if temp%2 == 0:
+                            print(f"\033[1m{k}\033[0m: {i[data.index(k)+1]}")
+                        else: print(f"\033[1m{k:<20}\033[0m: {i[data.index(k)+1]:<20}",end="")
+                    print("\n")
                 
             except:
                 print("\033[1minvalid input\033[0m ")
+            
 
-            for i in (result):
-                
-                for k in data:
-                    temp = data.index(k)+1
-                    if temp%2 == 0:
-                        print(f"\033[1m{k}\033[0m: {i[data.index(k)+1]}")
-                    else: print(f"\033[1m{k:<20}\033[0m: {i[data.index(k)+1]:<20}",end="")
-                print("\n")
+            
         
         elif choice == 'generate':
             numba = int(input("\033[1mHow many stars would you like to create?: \033[0m "))
             for i in range(numba):
                 star()
+            query = "select * from stars"
+            cursor.execute(query)
+            result = cursor.fetchall()
+            
+            linedata = [["Asteroid Belt","1000 miles","2000 miles","3000 miles","4000 miles","5000 miles","6000 miles","7000 miles","8000 miles","9000 miles","10000 miles"],
+["No atmosphere","Trace","Very thin, tainted","Very thin","Thin, tainted","Thin","Standard","Standard, tainted","Dense","Dense, tainted","Exotic","Corrosive","Insidious","Dense, high","Ellippsoid","Thin, low"],
+["No free standing water","10% water","20% water","30% water","40% water","50% water", "60% water","70% water","80% water","90% water", "No land masses"],
+["No inhabitants", "Tens of inhabitants", "Hundreds of inhabitants", "Thousands of inhabitants", "Hundreds of thousands of inhabitants", "Millions of inhabitants","Tens of millions of inhabitants","Hundreds of millions of inhabitants","Billions of inhabitants","Tens of billions of inhabitants"],
+['No government structure','Company/Corporation','Participating Democracy','Self-Perpetuating Oligarchy','Representative Democracy','Feudal Technocracy','Captive Government','Balkanization','Civil Service Bureaucracy','Impersonal Bureaucracy','Charismatic Dictator','Non-Charismatic Leader','Charismatic Oligarchy','Religious Dictatorship'],
+["No prohibitions", "Body pistols are undetectable by standard detectors, explosives and poison are prohibited", "Portable enrgy weapons are prohibited","Weapons of a strict military nature prohibited","Light assault weapons prohibited","Personal concealable firearms are prohibited", "Most firearms prohibited","Shotguns are prohibited", "Long bladed weapons are controlled, and open possesion is prohibited", "Posession of any weapon outside of one's resident is prohibited", "Weapon Possesion is prohibited"],
+['Stone Age. Primitive','Bronze to Middle Ages','1400-1700','1700-1860','1860-1900','1900-1939','1940-1969','1970-1979','1980-1989','1990-2000','Interstellar community','Average Imperial','Average Imperial','Above Average Imperial','Above Average Imperial','Technical maximum Imperial','Occasional non-Imperial'],
+["Excellent quality","Good quality","Routine quality","Poor quality","Frontier installation","No starport"]]
+
+            for i in (result):
+                for k in data:
+                    temp = data.index(k)+1
+                    if temp < 10:
+                        if temp%2 == 0:
+                            if temp == 2:
+                                print(f"\033[1m{k:>55}\033[0m: {i[data.index(k)+1]}, {linedata[data.index(k)-1][i[data.index(k)+1]]}")
+                            else:
+                                print(f"\033[1m{k}\033[0m: {i[data.index(k)+1]}, {linedata[data.index(k)-1][i[data.index(k)+1]]}")
+                        else: 
+                            if temp !=1:
+                                if temp ==9:
+                                    dic = {"A":0,"B":1,"C":2,"D":3,"E":4,"X":5}
+                                    print(f"\033[1m{k:<20}\033[0m: {i[data.index(k)+1]:<100}",f"{linedata[7][dic[i[9]]]:<50}", end="")
+                                    
+                                else:
+                                    print(f"\033[1m{k:<20}\033[0m: {i[data.index(k)+1]:<20}",f"{linedata[data.index(k)-1][i[data.index(k)+1]]:<50}", end="")
+
+                            else:
+                                print(f"\033[1m{k:<20}\033[0m: {i[data.index(k)+1]:<20}", end="")
+                                
+                print("\n")
 
 main()
